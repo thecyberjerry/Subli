e7f1798e7627b85671265488a46fa50f6bf1f112
Himanshu <72544756+thecyberjerry@users.noreply.github.com>
2022-12-15 17:56:52 +0530

    Update subli.py

--- subli.py
+++ subli.py
@@ -19,25 +19,13 @@ def subli(x,y):
     https://github.com/OWASP/Amass
     https://github.com/aboul3la/Sublist3r   
     """
+with open('Note.txt','r') as file2:
+    var3 = file2.read()
+print(Fore.GREEN,f'\n{var3}')
 
-print(Fore.GREEN,"""
-******************NOTE FROM DEVELOPER  [MUST READ]******************
-
-Please keep in mind the following points before using this Script:
-
-1. Please use the default list.txt provided in our Github Repository or refer to:
-                https://github.com/danielmiessler/SecLists
-                
-2. If using list other than default provided than paste the txt file in this python file directory renaming file to list.txt.
-
-3. Please Enter the Target in the provided format. For example: google.com,bing.com,target.com
-
-4. Last but not least it will only find subdomains available on https connection.
-
-""")
 if __name__ == '__main__':
 
-    var2 = input('Enter your target: ')
+    var2 = input(Fore.CYAN+'Enter your target: ')
 
     with open('list.txt','r') as file:
         var = file.read()
