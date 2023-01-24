97f2d74731aa775d60c0a0410a5f68ad226d6873
Himanshu <72544756+thecyberjerry@users.noreply.github.com>
2023-01-25 00:26:01 +0530

    Update subli.py

--- subli.py
+++ subli.py
@@ -1,34 +1,43 @@
 import requests
+import sys
+import argparse
 from colorama import Fore
+import threading
+import pyfiglet
+banner = pyfiglet.figlet_format("Subli")
+print(banner)
 
-def subli(x,y):
-    print(Fore.CYAN,'\n*********Scanning Started********\n',Fore.GREEN)
+def subli(x, y):
+    with open('readme.md','r') as file:
+        var = file.read()
+        print(var)
+    print(Fore.CYAN, '\n*********Scanning Started********\n', Fore.GREEN)
     for item in y:
         url = f"https://{item}.{x}"
         try:
-            print(Fore.YELLOW,url,Fore.GREEN)
-            requests.get(url,timeout=10)
-            print(Fore.RED +'[Working]-->',Fore.RESET,f'{url}',Fore.GREEN)
+            print(Fore.YELLOW, url, Fore.GREEN)
+            requests.get(url, timeout=10)
+            print(Fore.RED + '[Working]-->', Fore.RESET, f'{url}', Fore.GREEN)
         except requests.ConnectionError:
             pass
-    print(Fore.CYAN,'\n********Scanning Finished********',Fore.GREEN)
-    return"""
-    \nThanks for using this basic Python Subdomain and VHOST scanner.
-    
-    For more fine results refer to:\n
-    https://github.com/OWASP/Amass
-    https://github.com/aboul3la/Sublist3r   
-    """
-with open('Note.txt','r') as file2:
-    var3 = file2.read()
-print(Fore.GREEN,f'\n{var3}')
+        except requests.exceptions.InvalidURL:
+            pass
+        except KeyboardInterrupt as e:
+            e = '\n KeyBoard Interrupt Detected Exiting...'
+            print(e)
+            sys.exit()
+    print(Fore.CYAN, '\n********Scanning Finished********', Fore.GREEN)
 
-if __name__ == '__main__':
-
-    var2 = input(Fore.CYAN+'Enter your target: ')
-    
-    path = input(Fore.BLUE + 'Enter File Path: ' + Fore.RESET)
-    with open(f'{path}', 'r') as file:
-        var = file.read()
-        sub_dom = var.splitlines()
-    print(subli(var2, sub_dom))
+parser=argparse.ArgumentParser(
+    usage= '''dirfu.py -d example.com -f /path/to/file.txt''',
+    description='''DirFU a Directory Bruteforcer for beginners''',
+    epilog="""Developer: @instagram.com/thecyberjerry""")
+parser.add_argument('-d', '--domain', required=True, help='-d example.com')
+parser.add_argument('-f', '--file', required=True, help='-f /path/to/file/file.txt')
+args = parser.parse_args()
+Target = vars(args)
+with open(Target.get('file'), 'r') as file:
+    var2 = file.read()
+    x = var2.splitlines()
+threading = threading.Thread(target=subli(Target.get('domain'),x))
+threading.start()
